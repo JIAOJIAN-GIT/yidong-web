e2e4aca62b6fbead83f7ed330809b7840951c3ff
Jiao Jian <903939365@qq.com>
2020-08-01 09:58:37 +0800

    数据超出js安全整数范围处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
+import jsonBig from 'json-bigint'
 const request = axios.create({
   baseURL: 'http://localhost:8080/' // 基础路径
 })
+
+// 请求拦截
 request.interceptors.request.use(
   function (config) {
     return config
@@ -10,6 +13,8 @@ request.interceptors.request.use(
     return Promise.reject(error)
   }
 )
+
+// 响应拦截
 request.interceptors.response.use(
   function (response) {
     return response
@@ -18,5 +23,14 @@ request.interceptors.response.use(
     return Promise.reject(error)
   }
 )
+
+// 数据超出js安全整数范围处理
+request.defaults.transformResponse = [function (data) {
+  try {
+    return jsonBig.parse(data)
+  } catch {
+    return {}
+  }
+}]
 
 export default request
